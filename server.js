ba9bd006d33cacd8a70134f730d29573398e2454
andrewbalberchak <abalberchak@gmail.com>
2018-02-12 23:57:05 -0800

    3rd commit

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-require('./app/routes')(app, {});
 
-app.listen(port, () => {
-	console.log(`We are live on port: ${port}`);
-});
+MongoClient.connect(db.url, (err, database) => {
+	if (err) return console.log(err);
+	require('./app/routes')(app, database);
+	app.listen(port, () => {
+		console.log(`We are live on port: ${port}`);
+	});
+})
